cc400b188328d546555ae2cb23d537bf13b48b30
only1 <971159691@qq.com>
2019-06-26 22:36:27 +0800

    done6-26

---
+++ Python_matplotlib_Exercise/Exer2019_06_26.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.bar([1, 2, 3], [4, 5, 6], label = "Second line", color = 'g')
+plt.bar([6, 7, 4], [6, 7, 8], label = "First line")
+# plt.xlabel("imp")
+# plt.ylabel("int")
+# plt.title("interesting point")
+# plt.legend()
+plt.show()
